436ab1e4233f7ae655c1897928f2e54eb9a54ef9
Tharayil <ranjith.tharayil@accenture.com>
2018-04-13 21:38:55 +0530

    E2

---
+++ Example/2/testfirst/src/test/java/com/workshop/ctdd/testfirst/TemplateEngineStub.java
@@ -0,0 +1,16 @@
+package com.workshop.ctdd.testfirst;
+
+public class TemplateEngineStub implements Template {	
+	
+
+	public String evaluate(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void set(String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

9c8a43b780269845a65df6b638d24f8d1fdf4343
Tharayil <ranjith.tharayil@accenture.com>
2018-04-15 18:21:07 +0530

    added code base for TF

---
+++ workbook/8/hamarabasket/src/test/java/com/workshop/ctdd/tf/hamarabasket/AppTest.java
@@ -0,0 +1,24 @@
+package com.workshop.ctdd.tf.hamarabasket;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+
+{
+
+	@Test
+	public void aJunkTest() {
+
+		assertTrue(false);
+
+	}
+
+	@Test
+	public void AnotherJunkTest() {
+
+		assertTrue(true);
+
+	}
+}
